a7b51971856570e02e147bcd722fbcb6e440ae5d
Linnher <107924149+Linnher@users.noreply.github.com>
2022-06-30 09:29:17 +1200

    Add files via upload
    Skeleton of the Base Code

---
+++ 00_LUbase_v0.py
@@ -0,0 +1,15 @@
+
+#Base component for the game Lucky Unicorn
+#V0 - Skeleton Setup
+
+#Author - Linn Herzig
+#LH 2022
+
+
+# Import libraries below this line **************************
+
+# All Functions below this line ******************************
+
+# Main Routine below this line ******************************
+
+
